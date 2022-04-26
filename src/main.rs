40af0b2416f7584eda08e95c779316a4b0193605
Andrew Sulistio <Andrew@Raftesalo.net>
2022-04-25 23:46:30 -0700

    Initial example

---
+++ src/main.rs
@@ -0,0 +1,116 @@
+//! Simple in-memory key/value store showing features of axum.
+//!
+//! Run with:
+//!
+//! ```not_rust
+//! cargo run -p example-key-value-store
+//! ```
+
+use axum::{
+    body::Bytes,
+    error_handling::HandleErrorLayer,
+    extract::{ContentLengthLimit, Extension, Path},
+    handler::Handler,
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{post},
+    Router,
+};
+use std::{
+    borrow::Cow,
+    collections::HashMap,
+    net::SocketAddr,
+    sync::{Arc, Mutex},
+    time::Duration,
+};
+use tower::{BoxError, ServiceBuilder};
+use tower_http::{
+    auth::RequireAuthorizationLayer, compression::CompressionLayer, trace::TraceLayer,
+};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+use std::thread;
+
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::EnvFilter::new(
+            std::env::var("RUST_LOG")
+                .unwrap_or_else(|_| "example_key_value_store=debug,tower_http=debug".into()),
+        ))
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    // initialize our shared state
+    let state = Arc::new(Mutex::new(State::default()));
+
+    // Build our application by composing routes
+    let app = Router::new()
+        .route(
+            "/:key",
+            // Add compression to `kv_get`
+            post({
+                let shared_state = Arc::clone(&state);
+                move |path, body| kv_set(path, body, Arc::clone(&shared_state))
+            }),
+        )
+        .layer(
+            ServiceBuilder::new()
+                // Handle errors from middleware
+                .layer(HandleErrorLayer::new(handle_error))
+                .load_shed()
+                .concurrency_limit(1024)
+                .timeout(Duration::from_secs(10))
+                .layer(TraceLayer::new_for_http())
+                .into_inner(),
+        );
+
+    //spawn thread that will periodically print the shared state
+    thread::spawn(move || 
+        {
+            let ten_millis = Duration::from_millis(100);
+            let shared_state = Arc::clone(&state);
+            loop 
+            {
+                thread::sleep(ten_millis);
+                println!("{:?}", state.lock().unwrap().db);
+            }
+        });
+
+    // Run our app with hyper
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+#[derive(Default)]
+struct State {
+    db: HashMap<String, Bytes>,
+}
+
+async fn kv_set(Path(key): Path<String>, ContentLengthLimit(bytes): ContentLengthLimit<Bytes, { 1024 * 5_000 }>, state: Arc<Mutex<State>>) {
+    let mut state = state.lock().unwrap();
+    state.db.insert(key, bytes);
+}
+
+async fn handle_error(error: BoxError) -> impl IntoResponse {
+    if error.is::<tower::timeout::error::Elapsed>() {
+        return (StatusCode::REQUEST_TIMEOUT, Cow::from("request timed out"));
+    }
+
+    if error.is::<tower::load_shed::error::Overloaded>() {
+        return (
+            StatusCode::SERVICE_UNAVAILABLE,
+            Cow::from("service is overloaded, try again later"),
+        );
+    }
+
+    (
+        StatusCode::INTERNAL_SERVER_ERROR,
+        Cow::from(format!("Unhandled internal error: {}", error)),
+    )
+}
